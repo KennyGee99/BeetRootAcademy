77c291fe7d8aa03efd6b3ed6e6676095c324ae83
KennyGee99 <121250953+KennyGee99@users.noreply.github.com>
2022-12-22 18:06:07 +0100

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,4 @@
+# I'm beginning to learn 💻
+print('I\'m', 'beginning', 'to', 'learn', sep=' ', end= '💻' '\n' )
+# Hi👋there👋BeetRootAcademy!
+print('Hi', 'there', 'BeetRootAcademy', sep= '👋', end= '!')
